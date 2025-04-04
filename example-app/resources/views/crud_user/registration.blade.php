b1c369c8331915e8ef8a5e63b0030d6340e13c25
vukha12 <kha15964@gmail.com>
2025-04-04 09:08:34 +0700

    text

--- example-app/resources/views/crud_user/registration.blade.php
+++ example-app/resources/views/crud_user/registration.blade.php
@@ -9,7 +9,7 @@
         rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./css/styles.css" />
-    <title>Register</title>
+    <title>Registers</title>
 </head>
 
 <style>
